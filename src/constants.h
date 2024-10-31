82a07a02f52632c389fd85265f9012eb985383ef
Juan Rafael <gofiotelde@gmail.com>
2024-10-31 17:37:58 +0000

    CADENA 2.0V FINAL

--- src/constants.h
+++ src/constants.h
@@ -5,21 +5,22 @@ const int pinMotorTrabajo = 19;
 const int pinArranqueArriba = 21;
 const int pinArranqueAbajo = 23;
 const int pinFinCarreraArriba = 17;
-const int pinFinCarreraAbajo = 13;
+const int pinFinCarreraAbajo = 27;
 const int pinMando = 16;
 const int pinBotonSubirManual = 32;
 const int pinBotonBajarManual = 33;
 const int pinCorte = 4;
 const int pinSistemaAutomaticoAnulado = 22;
 const int pinLedError = 18;
-const int pinTipoMotor = 14;
+const int pinTipoMotor = 26;
 
-// Variable que controla cuanto tiempo tarda en bajar si la cadena esta subiendo y no llega arriba
-const unsigned long tiempoEntreSubirYBajar = 500; // 0.5 segundos
+// Variable que controla cuanto tiempo tarda en bajar si la cadena esta subiendo y no llega al final
+const unsigned long tiempoEntreSubirYBajar = 500; // 0.5 segundos, este tiempo es para cuando no detescta finArriba para motor durante este tiempo antes de encender relé de bajada para que
+// no se produzca un corto en los reles, aunque con los reles de 11 pines se solucionó y no sería necesario.
 
-const unsigned long tiempoMaximoTrabajo = 6000;       // 6 segundos
+const unsigned long tiempoMaximoTrabajo = 30000;      // 9.5 segundos, para maqueta 30000
 const unsigned long tiempoTrabajoArranque = 500;      // 0.5 segundos
-const unsigned long tiempoMaximoCadenaBajada = 120000; // 2 minutos
+const unsigned long tiempoMaximoCadenaBajada = 30000; // 2 minutos
 
 const unsigned long tiempoMandoPresionado = 600;    // 0.6 segundos
-const unsigned long tiempoIgnorarEnArranque = 1500; //  1.5 segundos
+const unsigned long tiempoIgnorarEnArranque = 1500; //  1.5 segundos, para maqueta   7000
--- src/main.cpp
+++ src/main.cpp
@@ -1,155 +1,188 @@
-#include <Arduino.h>
-#include <elapsedMillis.h>
+#include <Arduino.h>       // Librería principal de Arduino para manejar pines, comunicación serial, etc.
+#include <elapsedMillis.h> // Librería que permite rastrear el tiempo transcurrido fácilmente
 
-#include "constants.h"
-#include "EstadoCadena.h"
-#include "EstadoMotor.h"
+#include "constants.h"    // Incluye constantes definidas externamente
+#include "EstadoCadena.h" // Incluye definiciones y estados de la cadena
+#include "EstadoMotor.h"  // Incluye definiciones y estados del motor
 
-bool error = false;
-bool buttonPressed = false; // Añade esta línea
-bool accionRealizada = false;
+// Variables globales de estado y configuración
+bool error = false;           // Variable para indicar si hay un error
+bool buttonPressed = false;   // Indica si el botón ha sido presionado
+bool accionRealizada = false; // Indica si se ha realizado una acción basada en la pulsación del botón
 
-// tipo Motor
-bool isTrifasico = true;
+// Configuración del tipo de motor
+bool isTrifasico = true; // true = trifásico, false = monofásico
 
-// cadena
-bool cadenaBajada = false;
+// Estado de la cadena
+bool cadenaBajada = false; // Indica si la cadena está en posición de bajada
 
-// error
-bool ledErrorEncendido = false;
+// Estado del LED de error
+bool ledErrorEncendido = false; // Variable para controlar el estado del LED de error
 
-elapsedMillis mandoPulsado;         // Tiempo transcurrido desde la última vez que el pinMando fue pulsado
-elapsedMillis tiempoMotorEncendido; // Tiempo transcurrido desde que se arrancó el motor
-elapsedMillis tiempoCadenaBajada;
+// Objetos de elapsedMillis para rastrear tiempos específicos
+elapsedMillis mandoPulsado;         // Tiempo desde la última vez que se pulsó el mando
+elapsedMillis tiempoMotorEncendido; // Tiempo desde que el motor comenzó a funcionar
+elapsedMillis tiempoCadenaBajada;   // Tiempo desde que la cadena está en bajada
 
+// Variable para almacenar el estado actual del motor
 EstadoMotor estadoMotor;
 
+// Función para obtener el estado de la cadena en función de los sensores de fin de carrera
 EstadoCadena obtenerEstadoCadena()
 {
-  bool finCarreraArriba = digitalRead(pinFinCarreraArriba) == HIGH;
-  bool finCarreraAbajo = digitalRead(pinFinCarreraAbajo) == HIGH;
+  bool finCarreraArriba = digitalRead(pinFinCarreraArriba) == HIGH; // Verifica si el sensor de fin de carrera arriba está activado
+  bool finCarreraAbajo = digitalRead(pinFinCarreraAbajo) == HIGH;   // Verifica si el sensor de fin de carrera abajo está activado
 
+  // Retorna el estado correspondiente según los sensores de fin de carrera
   if (finCarreraArriba && finCarreraAbajo)
   {
-    return AMBOS;
+    return AMBOS; // Ambos sensores están activados
   }
   else if (finCarreraArriba)
   {
-    return ARRIBA;
+    return ARRIBA; // Solo el sensor de arriba está activado
   }
   else if (finCarreraAbajo)
   {
-    return ABAJO;
+    return ABAJO; // Solo el sensor de abajo está activado
   }
   else
   {
-    return NINGUNO;
+    return NINGUNO; // Ningún sensor está activado
   }
 }
 
+// Función para apagar el motor y establecer su estado en APAGADO
 void apagarMotor()
 {
-  Serial.println("Apagando motor");
-  digitalWrite(pinArranqueArriba, LOW);
-  digitalWrite(pinArranqueAbajo, LOW);
-  digitalWrite(pinMotorTrabajo, LOW);
-  estadoMotor = APAGADO;
+  digitalWrite(pinArranqueArriba, LOW); // Apaga el pin de arranque hacia arriba
+  digitalWrite(pinArranqueAbajo, LOW);  // Apaga el pin de arranque hacia abajo
+  digitalWrite(pinMotorTrabajo, LOW);   // Apaga el pin de trabajo del motor
+  estadoMotor = APAGADO;                // Cambia el estado del motor a APAGADO
 }
 
+// Tiempo de arranque del motor
 elapsedMillis tiempoArranqueMotor;
+
+// Función para arrancar el motor en una dirección específica
 void arrancarMotor(bool haciaArriba)
 {
-  cadenaBajada = false;
+  cadenaBajada = false; // Marca que la cadena ya no está en bajada
   if (haciaArriba)
   {
-    Serial.println("Arrancando motor hacia arriba");
-    digitalWrite(pinArranqueArriba, HIGH);
-    digitalWrite(pinArranqueAbajo, LOW);
+    Serial.println("Arrancando motor hacia arriba"); // Mensaje indicando arranque hacia arriba
+    digitalWrite(pinArranqueArriba, HIGH);           // Activa el pin de arranque en dirección arriba
+    digitalWrite(pinArranqueAbajo, LOW);             // Asegura que el pin de arranque en dirección abajo esté desactivado
     if (!isTrifasico)
     {
-      digitalWrite(pinMotorTrabajo, HIGH);
+      digitalWrite(pinMotorTrabajo, HIGH); // Activa el pin de trabajo si el motor es monofásico
     }
-
-    estadoMotor = SUBIENDO;
+    estadoMotor = SUBIENDO; // Actualiza el estado del motor a SUBIENDO
   }
   else
   {
-    Serial.println("Arrancando motor hacia abajo");
-    digitalWrite(pinArranqueArriba, LOW);
-    digitalWrite(pinArranqueAbajo, HIGH);
+    Serial.println("Arrancando motor hacia abajo"); // Mensaje indicando arranque hacia abajo
+    digitalWrite(pinArranqueArriba, LOW);           // Asegura que el pin de arranque en dirección arriba esté desactivado
+    digitalWrite(pinArranqueAbajo, HIGH);           // Activa el pin de arranque en dirección abajo
     if (!isTrifasico)
     {
-      digitalWrite(pinMotorTrabajo, HIGH);
+      digitalWrite(pinMotorTrabajo, HIGH); // Activa el pin de trabajo si el motor es monofásico
     }
-    estadoMotor = BAJANDO;
+    estadoMotor = BAJANDO; // Actualiza el estado del motor a BAJANDO
   }
-  tiempoMotorEncendido = 0;
-  delay(400); // tiempo para ignorar ruido de reles.
+  tiempoMotorEncendido = 0; // Resetea el temporizador del motor encendido
+  delay(400);               // Pausa breve para evitar ruidos de los relés
 }
 
-// Controla la funcionalidad de mantener el mando pulsado
+// Función para gestionar la pulsación del mando y ejecutar acciones en consecuencia
 void gestionMando()
 {
-
-  if (digitalRead(pinMando) == HIGH) // Si el pinMando está en HIGH
+  if (digitalRead(pinMando) == HIGH) // Si el pin de mando está en alto, indica pulsación
   {
-    if (!buttonPressed) // Si el botón no estaba pulsado
+    if (!buttonPressed) // Si el botón no estaba registrado como pulsado previamente
     {
-      buttonPressed = true; // Marca el botón como pulsado
-      mandoPulsado = 0;     // Resetea el tiempo transcurrido
-      accionRealizada = false;
+      buttonPressed = true;    // Marca el botón como pulsado
+      mandoPulsado = 0;        // Resetea el tiempo transcurrido desde la última pulsación
+      accionRealizada = false; // Resetea la acción realizada para permitir una nueva acción
     }
   }
   else
   {
-    buttonPressed = false; // Marca el botón como no pulsado
+    buttonPressed = false; // Marca el botón como no pulsado si el pinMando está en LOW
   }
 
-  if (mandoPulsado > tiempoMandoPresionado && buttonPressed && !accionRealizada) // Si han pasado más de 1000ms desde que el botón fue pulsado
+  // Ejecuta acción si el mando ha estado presionado el tiempo necesario
+  if (mandoPulsado > tiempoMandoPresionado && buttonPressed && !accionRealizada)
   {
-    EstadoCadena estadoCadena = obtenerEstadoCadena();
+    EstadoCadena estadoCadena = obtenerEstadoCadena(); // Obtiene el estado actual de la cadena
 
-    Serial.println("Botón pulsado durante más de 600ms");
+    Serial.println("Botón pulsado durante más de 600ms"); // Indica que el botón fue pulsado suficientemente
 
-    if (estadoCadena == ARRIBA)
+    if (estadoCadena == ARRIBA) // Si la cadena está en la posición ARRIBA
     {
       Serial.println("Cadena arriba");
-      arrancarMotor(false);
+      arrancarMotor(false); // Arranca el motor hacia abajo
     }
-    else if (estadoCadena == ABAJO)
+    else if (estadoCadena == ABAJO) // Si la cadena está en la posición ABAJO
     {
       Serial.println("Cadena abajo");
-      arrancarMotor(true);
+      arrancarMotor(true); // Arranca el motor hacia arriba
     }
-    accionRealizada = true;
+    accionRealizada = true; // Marca que la acción ha sido realizada
   }
 }
 
+// Función para leer y procesar los sensores de fin de carrera
 void leerFinesCarrera()
 {
-  EstadoCadena estadoCadena = obtenerEstadoCadena();
-  if (estadoCadena == ARRIBA || estadoCadena == ABAJO && digitalRead(pinCorte) == HIGH)
+  EstadoCadena estadoCadena = obtenerEstadoCadena(); // Obtiene el estado actual de la cadena
+
+  // Nueva condición de error: ambos fines de carrera activados
+  if (estadoCadena == AMBOS)
   {
-    error = false;
+    Serial.println("Error: Ambos fines de carrera activados"); // Mensaje de error
+    error = true;                                              // Activa el estado de error
+    digitalWrite(pinLedError, HIGH);                           // Enciende el LED de error
+    ledErrorEncendido = true;                                  // Marca el LED de error como encendido
+    apagarMotor();                                             // Apaga el motor
   }
+
+  // Condición de fin de carrera ARRIBA activado y motor subiendo
   if (estadoCadena == ARRIBA && estadoMotor == SUBIENDO)
   {
     Serial.println("Fin de carrera arriba");
-    apagarMotor();
+    apagarMotor(); // Apaga el motor si la cadena está arriba y el motor subiendo
   }
+
+  // Condición de fin de carrera ABAJO activado y motor bajando
   if (estadoCadena == ABAJO && estadoMotor == BAJANDO)
   {
     Serial.println("Fin de carrera abajo");
-    cadenaBajada = true;
-    tiempoCadenaBajada = 0;
-    apagarMotor();
+    cadenaBajada = true;    // Marca que la cadena está en posición de bajada
+    tiempoCadenaBajada = 0; // Resetea el temporizador de cadena bajada
+    apagarMotor();          // Apaga el motor si la cadena está abajo y el motor bajando
   }
 }
 
+// Función para verificar si el error se ha subsanado
+void verificarError()
+{
+  // Verifica si el error se puede subsanar:
+  // El pinCorte debe estar en HIGH, y solo uno o ninguno de los sensores de fin de carrera debe estar activo
+  if (digitalRead(pinCorte) == HIGH && obtenerEstadoCadena() != AMBOS)
+  {
+    error = false;                  // Desactiva el estado de error
+    digitalWrite(pinLedError, LOW); // Apaga el LED de error
+    ledErrorEncendido = false;      // Marca el LED como apagado
+  }
+}
+
+// Configuración inicial de pines y estado en el setup
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(115200); // Inicializa la comunicación serial a 115200 baudios
 
+  // Configura los pines de entrada con resistencia de pull-down
   pinMode(pinMando, INPUT_PULLDOWN);
   pinMode(pinFinCarreraArriba, INPUT_PULLDOWN);
   pinMode(pinFinCarreraAbajo, INPUT_PULLDOWN);
@@ -159,39 +192,44 @@ void setup()
   pinMode(pinBotonBajarManual, INPUT_PULLDOWN);
   pinMode(pinTipoMotor, INPUT_PULLDOWN);
 
+  // Configura los pines de salida
   pinMode(pinMotorTrabajo, OUTPUT);
   pinMode(pinArranqueArriba, OUTPUT);
   pinMode(pinArranqueAbajo, OUTPUT);
   pinMode(pinLedError, OUTPUT);
 
+  // Lee el tipo de motor al inicio
   isTrifasico = digitalRead(pinTipoMotor) == HIGH ? false : true;
   Serial.println("Finalizado Setup");
 
+  // Si no se detecta ningún fin de carrera, el motor baja
   if (obtenerEstadoCadena() == NINGUNO)
   {
     arrancarMotor(false);
   }
 }
 
+// Manejo de arranque manual en caso de error
 elapsedMillis tiempoArranqueManual;
+
 void arrancarMotorError(bool haciaArriba)
 {
   if (haciaArriba)
   {
-    Serial.println("Arrancando motor hacia arriba");
-    digitalWrite(pinArranqueArriba, HIGH);
-    digitalWrite(pinArranqueAbajo, LOW);
-    tiempoArranqueManual = 0;
+    Serial.println("Arrancando motor hacia arriba"); // Mensaje de arranque hacia arriba
+    digitalWrite(pinArranqueArriba, HIGH);           // Activa arranque en dirección arriba
+    digitalWrite(pinArranqueAbajo, LOW);             // Asegura que el pin de arranque hacia abajo esté desactivado
+    tiempoArranqueManual = 0;                        // Resetea el temporizador de arranque manual
     if (!isTrifasico)
     {
-      digitalWrite(pinMotorTrabajo, HIGH);
+      digitalWrite(pinMotorTrabajo, HIGH); // Activa el pin de trabajo si el motor es monofásico
     }
   }
   else
   {
-    Serial.println("Arrancando motor hacia abajo");
-    digitalWrite(pinArranqueArriba, LOW);
-    digitalWrite(pinArranqueAbajo, HIGH);
+    Serial.println("Arrancando motor hacia abajo"); // Mensaje de arranque hacia abajo
+    digitalWrite(pinArranqueArriba, LOW);           // Asegura que el pin de arranque hacia arriba esté desactivado
+    digitalWrite(pinArranqueAbajo, HIGH);           // Activa arranque en dirección abajo
     tiempoArranqueManual = 0;
     if (!isTrifasico)
     {
@@ -201,110 +239,115 @@ void arrancarMotorError(bool haciaArriba)
 
   if (!isTrifasico)
   {
-    while (
-        (digitalRead(pinBotonSubirManual) == HIGH || digitalRead(pinBotonBajarManual) == HIGH) &&
-        tiempoArranqueManual < tiempoTrabajoArranque)
+    // Mientras esté presionado el botón y el tiempo de trabajo no se exceda
+    while ((digitalRead(pinBotonSubirManual) == HIGH || digitalRead(pinBotonBajarManual) == HIGH) && tiempoArranqueManual < tiempoTrabajoArranque)
     {
+      // Espera hasta que termine el tiempo de trabajo
     }
-    digitalWrite(pinArranqueArriba, LOW);
-    digitalWrite(pinArranqueAbajo, LOW);
+    digitalWrite(pinArranqueArriba, LOW); // Apaga el arranque arriba
+    digitalWrite(pinArranqueAbajo, LOW);  // Apaga el arranque abajo
   }
 }
 
+// Función que gestiona el arranque manual utilizando botones de subida y bajada
 void gestionarArranqueManual()
 {
   if (digitalRead(pinBotonSubirManual) == HIGH)
   {
-    arrancarMotorError(true);
+    arrancarMotorError(true); // Arranca el motor hacia arriba en caso de error
     while (digitalRead(pinBotonSubirManual) == HIGH)
     {
-    }
-
-    apagarMotor();
+    }              // Espera a que se suelte el botón
+    apagarMotor(); // Apaga el motor una vez soltado el botón
   }
   else if (digitalRead(pinBotonBajarManual) == HIGH)
   {
-    arrancarMotorError(false);
+    arrancarMotorError(false); // Arranca el motor hacia abajo en caso de error
     while (digitalRead(pinBotonBajarManual) == HIGH)
     {
-    }
-
-    apagarMotor();
+    }              // Espera a que se suelte el botón
+    apagarMotor(); // Apaga el motor una vez soltado el botón
   }
 }
 
+// Bucle principal de ejecución del código
 void loop()
 {
-  if (digitalRead(pinCorte) == LOW)
+  if (digitalRead(pinCorte) == LOW) // Si el pinCorte está bajo, indica un estado de error
   {
-    apagarMotor();
-    error = true;
-    if (digitalRead(pinLedError) == LOW)
+    apagarMotor();          // Apaga el motor en caso de error
+    error = true;           // Activa el estado de error
+    if (!ledErrorEncendido) // Si el LED de error no está encendido
     {
-      digitalWrite(pinLedError, HIGH);
+      digitalWrite(pinLedError, HIGH); // Enciende el LED de error
+      ledErrorEncendido = true;        // Marca el LED de error como encendido
     }
   }
 
-  if (error)
+  if (error) // Si el estado de error está activo
   {
-    if (!ledErrorEncendido)
+    gestionarArranqueManual(); // Permite el arranque manual en estado de error
+    verificarError();          // Verifica si el error se ha resuelto
+  }
+  else // Si no hay error activo
+  {
+    if (ledErrorEncendido) // Si el LED de error está encendido
     {
-      digitalWrite(pinLedError, HIGH);
+      digitalWrite(pinLedError, LOW);     // Apaga el LED de error
+      ledErrorEncendido = false;          // Marca el LED de error como apagado
+      if (obtenerEstadoCadena() == ABAJO) // Si la cadena está abajo
+      {
+        tiempoCadenaBajada = 0; // Resetea el temporizador de cadena bajada
+        cadenaBajada = true;    // Marca que la cadena está en posición de bajada
+      }
+    }
+
+    gestionMando(); // Gestiona el mando para posibles acciones de usuario
+
+    EstadoCadena estadoCadena = obtenerEstadoCadena(); // Obtiene el estado actual de la cadena
+
+    // Si el motor está subiendo o bajando, el motor no es trifásico y se excede el tiempo de arranque
+    if ((estadoMotor == SUBIENDO || estadoMotor == BAJANDO) && !isTrifasico && tiempoMotorEncendido > tiempoTrabajoArranque)
+    {
+      digitalWrite(pinArranqueAbajo, LOW);  // Desactiva el arranque hacia abajo
+      digitalWrite(pinArranqueArriba, LOW); // Desactiva el arranque hacia arriba
+    }
+
+    // Si la cadena está en estado NINGUNO y el motor está apagado, entra en error
+    if (estadoCadena == NINGUNO && estadoMotor == APAGADO)
+    {
+      error = true;                    // Activa el estado de error
+      digitalWrite(pinLedError, HIGH); // Enciende el LED de error
       ledErrorEncendido = true;
     }
 
-    gestionarArranqueManual();
-  }
-  else
-  {
-    // si el led error esta encendido, lo apagamos
-    if (ledErrorEncendido)
-    {
-      digitalWrite(pinLedError, LOW);
-      ledErrorEncendido = false;
-      if (obtenerEstadoCadena() == ABAJO)
-      {
-        tiempoCadenaBajada = 0;
-        cadenaBajada = true;
-      }
-    }
-    gestionMando();
-
-    EstadoCadena estadoCadena = obtenerEstadoCadena();
-
-    if ((estadoMotor == SUBIENDO || estadoMotor == BAJANDO) && !isTrifasico && tiempoMotorEncendido > tiempoTrabajoArranque)
-    {
-      digitalWrite(pinArranqueAbajo, LOW);
-      digitalWrite(pinArranqueArriba, LOW);
-    }
-
-    if (estadoCadena == NINGUNO && estadoMotor == APAGADO)
-    {
-      error = true;
-    }
-
+    // Verifica si la cadena ha estado en posición de bajada demasiado tiempo sin activación manual
     if (estadoCadena == ABAJO && estadoMotor == APAGADO && tiempoCadenaBajada > tiempoMaximoCadenaBajada && digitalRead(pinSistemaAutomaticoAnulado) == LOW)
     {
       Serial.println("Tiempo cadena bajada excedido, subiendo automaticamente");
-      arrancarMotor(true);
+      arrancarMotor(true); // Arranca el motor hacia arriba automáticamente
     }
 
-    if (tiempoMotorEncendido > (tiempoMaximoTrabajo) && !error && (estadoMotor == SUBIENDO || estadoMotor == BAJANDO))
+    // Verifica si se excede el tiempo máximo de trabajo del motor
+    if (tiempoMotorEncendido > tiempoMaximoTrabajo && !error && (estadoMotor == SUBIENDO || estadoMotor == BAJANDO))
     {
       if (estadoMotor == SUBIENDO)
       {
-        Serial.println("Tiempo máximo de trabajo alcanzado, y ademas subiendo, bajando automaticamente");
-        apagarMotor();
-        delay(500);
-        arrancarMotor(false);
+        Serial.println("Tiempo máximo de trabajo alcanzado, bajando automaticamente");
+        apagarMotor();                 // Apaga el motor
+        delay(tiempoEntreSubirYBajar); // Pausa breve
+        arrancarMotor(false);          // Inicia bajada automáticamente
       }
       else
       {
         Serial.println("Tiempo máximo de trabajo alcanzado");
-        apagarMotor();
+        error = true;                    // Activa el estado de error
+        digitalWrite(pinLedError, HIGH); // Enciende el LED de error
+        ledErrorEncendido = true;
+        apagarMotor(); // Apaga el motor si estaba bajando
       }
     }
   }
 
-  leerFinesCarrera();
+  leerFinesCarrera(); // Verifica los sensores de fin de carrera
 }
